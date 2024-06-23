447139d90c5f0659c26dd48e40715250c39b5a83
Luis Manuel Brito <lsbrt4@gmail.com>
2024-06-23 15:53:53 -0400

    primer paso, renderizo las peliculas y agrego el input

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import responseMovies from './mocks/with-result.json'
+import withoutResults from './mocks/no-result.json'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const movies = responseMovies.Search
+  const hasMovies= movies?.length > 0
   return (
-    <>
+    <div className='page'>
     <header>
       <h1>Buscador de Peliculas</h1>
       <form className='form' action="">
@@ -17,9 +16,28 @@ function App() {
     </header>
 
     <main>
-      Aqui van los resultados
+      {
+        hasMovies
+        ? (
+          <ul>
+            {
+              movies.map(movie=>(
+                <li key={movie.imdbID}>
+                  <h3>{movie.Title}</h3>
+                  <p>{movie.Year}</p>
+                  <img src={movie.Poster} alt={movie.Title} />
+                </li>
+              ))
+            }
+          </ul>
+        )
+        : (
+          <p>No se encontraron peliculas</p>
+        )
+      
+      }
     </main>
-    </>
+    </div>
   )
 }
 
