6175576d26acc628768c7c0fc12a9b103264ddf0
Luis Manuel Brito <lsbrt4@gmail.com>
2024-06-27 16:07:06 -0400

    creo las validaciones del formulario y lo hago hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,72 @@
 import './App.css'
-import { useRef } from 'react'
-
 import { useMovies } from './hooks/useMovies'
 import { Movies } from './components/Movies'
+import { useEffect, useState } from 'react'
+import { useRef } from 'react'
 
+function useSearch () {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
 
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, updateSearch, error }
+}
 
 function App() { 
 const { movies: mappedMovies } = useMovies()
-const inputRef = useRef()
+const { search, updateSearch, error } = useSearch()
+
 
 const handleSubmit = (event) => {
+  //sin necesidad de useRef consigo los inpus de n formularios si los quisiera
   event.preventDefault()
-  const inputEl = inputRef.current
-  const value = inputEl.value
-  console.log(value)
+/*   const fields = Object.fromEntries(new window.FormData(event.target))
+  const {query} = Object.fromEntries(new window.FormData(event.target)) */
+
+  console.log({query})
 }
 
+const handleChange = (event) => {
+  updateSearch(event.target.value)
+}
   return (
     <div className='page'>
     <header>
       <h1>Buscador de Peliculas</h1>
       <form className='form' action="" onSubmit={handleSubmit}>
-          <input  ref={inputRef} type="text" />
+          <input
+            style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent'
+            }} 
+          onChange={handleChange} value={search} name='query' type="text" />
           <button type='submit'>Buscar</button>
       </form>
+      {error && <p style={{color: 'red'}}>{error}</p>}
     </header>
 
     <main>
